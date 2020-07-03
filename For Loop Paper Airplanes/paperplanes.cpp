8c77f98e76f883738b3cb81e187a39434b4e879e
von-stardust <62289670+von-stardust@users.noreply.github.com>
2020-07-04 02:03:52 +0800

    Paper Airplanes For Loop

---
+++ For Loop Paper Airplanes/paperplanes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+
+  for (int i = 0; i < 10; i++) 
+
+/*
+There are three separate parts to this for condition separated by ;  :
+The initialization of a counter: int i = 0
+The continue condition: i < 20
+The change in the counter (in this case an increment): i++
+*/
+  
+  
+  {
+    
+    std::cout << "I will not throw paper airplanes in class.\n";
+    
+  }
+  system("pause");  
+   return 0;
+}
+
+/*
+ We are creating a variable i that starts from 0. 
+ We will repeat the code inside over and over again when i is less than 20. 
+ At the end the for loop, we are adding 1 to i using the ++ operator.
+*/
